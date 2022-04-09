d691740cf1fcfe603c1e7baa2eb34bf33a4e7175
Вадим Сухинин <suhinin.vadim2002@yandex.ru>
2022-04-09 12:56:18 +0500

    Урок 6, начало. Создали структуру матрицы

--- lab2engraph.cpp
+++ lab2engraph.cpp
@@ -6,7 +6,43 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-int main()
-{
+void RenderSceneCB() {
 
+	glClear(GL_COLOR_BUFFER_BIT);
+
+
+	glutSwapBuffers();
+}
+
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+
+	glutInitWindowSize(1024, 768);
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow("Lab 2");
+
+	glutDisplayFunc(RenderSceneCB);
+
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+	GLenum res = glewInit();
+	if (res != GLEW_OK)
+	{
+		fprintf(stderr, "Error: '%s'\n", glewGetErrorString(res));
+		return 1;
+	}
+
+	////////lesson 6//////////
+	struct Matrix4f {
+		glm::mat4x4 m{};
+	};
+
+	GLuint gWorldLocation;
+	/////////////////////////
+	
+
+	glutMainLoop();
 }
