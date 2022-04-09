364e5df2aa0b3ad3092507bf034bee71088759c0
Вадим Сухинин <suhinin.vadim2002@yandex.ru>
2022-04-09 12:26:59 +0500

    Добавьте файлы проекта.

---
+++ lab2engraph.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+int main()
+{
+
+}
