69ebba3e6059a1a426885c393792b807fbb5febe
Вадим Сухинин <suhinin.vadim2002@yandex.ru>
2022-04-15 13:07:54 +0500

    Правки в методах Pipeline

--- lab2engraph.cpp
+++ lab2engraph.cpp
@@ -68,26 +68,25 @@ public:
 		m_transformation[3][0] = 0.0f;	m_transformation[3][1] = 0.0f;	m_transformation[3][2] = 0.0f;	m_transformation[3][3] = 1.0f;
 	}
 
-	void Scale(float ScaleX, float ScaleY, float ScaleZ)
-	{
-		m_scale[0] = ToRadian(sqrt(1 - pow (ScaleX, 2)));	// сделали косинус
-		m_scale[1] = ToRadian(ScaleY);
-		m_scale[2] = ToRadian(ScaleZ);
-	}
-
 	void WorldPos(float x, float y, float z)
 	{
 		m_worldPos[0] = x;
 		m_worldPos[1] = y;
 		m_worldPos[2] = z;
 	}
-
 	void Rotate(float RotateX, float RotateY, float RotateZ)
 	{
-		m_rotateInfo[0] = RotateX;
-		m_rotateInfo[1] = RotateY;
-		m_rotateInfo[2] = RotateZ;
+		m_rotateInfo[0] = ToRadian(RotateX);		// синус
+		m_rotateInfo[1] = ToRadian(RotateY);		// минус синус
+		m_rotateInfo[2] = ToRadian(RotateZ);		// косинус
 	}
+	void Scale(float ScaleX, float ScaleY, float ScaleZ)
+	{
+		m_scale[0] = ToRadian(ScaleX);
+		m_scale[1] = ToRadian(ScaleY);				// Y должен умножаться на косинус, так что в кач-ве ScaleY нужно передать косинус
+		m_scale[2] = ToRadian(ScaleZ);
+	}
+
 
 	const glm::mat4x4* GetTrans();
 	void InitScaleTransform(glm::mat4x4& m);
@@ -99,21 +98,6 @@ private:
 	glm::vec3 m_rotateInfo;
 	glm::mat4x4 m_transformation;
 };
-void Pipeline::InitScaleTransform(glm::mat4x4& m) {
-	m[0][0] = m_scale[0];	m[0][1] = 0.0f;			m[0][2] = 0.0f;			m[0][3] = 0.0f;
-	m[1][0] = 0.0f;			m[1][1] = m_scale[1];	m[1][2] = 0.0f;			m[1][3] = 0.0f;
-	m[2][0] = 0.0f;			m[2][1] = 0.0f;			m[2][2] = m_scale[2];	m[2][3] = 0.0f;
-	m[3][0] = 0.0f;			m[3][1] = 0.0f;			m[3][2] = 0.0f;			m[3][3] = 1.0f;
-}
-void Pipeline::InitRotateTransform(glm::mat4x4& mm) {
-	glm::mat4x4 m;
-	m[0][0] = m_rotateInfo[0];						m[0][1] = -m_rotateInfo[1];						m[0][2] = 0.0f;		m[0][3] = 0.0f;
-	m[1][0] = m_rotateInfo[1];						m[1][1] = m_rotateInfo[0];						m[1][2] = 0.0f;		m[1][3] = 0.0f;
-	m[2][0] = 0.0f;									m[2][1] = 0.0f;									m[2][2] = 0.0f;		m[2][3] = 0.0f;
-	m[3][0] = 0.0f;									m[3][1] = 0.0f;									m[3][2] = 0.0f;		m[3][3] = 1.0f;
-
-	mm = mm * m;
-}
 void Pipeline::InitTranslationTransform(glm::mat4x4& m) {
 
 	m[0][0] = 1.0f; m[0][1] = 0.0f; m[0][2] = 0.0f; m[0][3] = m_worldPos[0];
@@ -121,9 +105,25 @@ void Pipeline::InitTranslationTransform(glm::mat4x4& m) {
 	m[2][0] = 0.0f; m[2][1] = 0.0f; m[2][2] = 1.0f; m[2][3] = m_worldPos[2];
 	m[3][0] = 0.0f; m[3][1] = 0.0f; m[3][2] = 0.0f; m[3][3] = 1.0f;
 }
+void Pipeline::InitRotateTransform(glm::mat4x4& m) {
+
+	m[0][0] = m_rotateInfo[2];						m[0][1] = m_rotateInfo[1];						m[0][2] = 0.0f;		m[0][3] = 0.0f;
+	m[1][0] = m_rotateInfo[0];						m[1][1] = m_rotateInfo[0];						m[1][2] = 0.0f;		m[1][3] = 0.0f;
+	m[2][0] = 0.0f;									m[2][1] = 0.0f;									m[2][2] = 1.0f;		m[2][3] = 0.0f;
+	m[3][0] = 0.0f;									m[3][1] = 0.0f;									m[3][2] = 0.0f;		m[3][3] = 1.0f;
+
+}
+void Pipeline::InitScaleTransform(glm::mat4x4& m) {
+	m[0][0] = m_scale[0];	m[0][1] = 0.0f;			m[0][2] = 0.0f;			m[0][3] = 0.0f;
+	m[1][0] = 0.0f;			m[1][1] = m_scale[1];	m[1][2] = 0.0f;			m[1][3] = 0.0f;
+	m[2][0] = 0.0f;			m[2][1] = 0.0f;			m[2][2] = m_scale[2];	m[2][3] = 0.0f;
+	m[3][0] = 0.0f;			m[3][1] = 0.0f;			m[3][2] = 0.0f;			m[3][3] = 1.0f;
+}
+
+
 const glm::mat4x4* Pipeline::GetTrans()
 {
-	glm::mat4x4 ScaleTrans, RotateTrans, TranslationTrans = glm::mat4x4 {0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,1};
+	glm::mat4x4 ScaleTrans, RotateTrans, TranslationTrans = glm::mat4x4 {1,0,0,0, 0,1,0,0, 0,0,1,0, 0,0,0,1};
 	
 	InitScaleTransform(ScaleTrans);
 	InitRotateTransform(RotateTrans);
@@ -137,13 +137,13 @@ void RenderSceneCB() {
 
 	glClear(GL_COLOR_BUFFER_BIT);
 
-	static float Scale = 0.0f;
+	static float Scale = 0.5f;
 	Scale += 0.01f;
 
 	Pipeline p;
-	p.Scale(sinf(Scale * 0.1f), sinf(Scale * 0.1f), sinf(Scale * 0.1f));
+	p.Scale(sinf(Scale * 0.1f), cosf(Scale * 0.1f), sinf(Scale * 0.1f));
 	p.WorldPos(sinf(Scale), 0.0f, 0.0f);
-	p.Rotate(sinf(Scale) * 90.0f, sinf(Scale) * 90.0f, sinf(Scale) * 90.0f);
+	p.Rotate(sinf(Scale) * 90.0f, -sinf(Scale) * 90.0f, cosf(Scale) * 90.0f);
 	glUniformMatrix4fv(gWorldLocation, 1, GL_TRUE, (const GLfloat*)p.GetTrans());
 
 
