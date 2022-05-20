5abe3c823af896cf3b5880d36004de65e8a2c705
Вадим Сухинин <suhinin.vadim2002@yandex.ru>
2022-05-20 13:25:56 +0500

    done

--- lab2engraph.cpp
+++ lab2engraph.cpp
@@ -18,10 +18,11 @@ GLuint renderingProgram;
 GLuint vao[numVAOs];
 GLuint vbo[numVBOs];
 // allocate variables used in display() function, so that they won’t need to be allocated during rendering
-GLuint mvLoc, projLoc;
+//GLuint mvLoc, projLoc;
+GLuint vLoc, tfLoc, projLoc;
 int width, height, displayLoopi;
 float aspect;
-float tf;
+float timeFactor;
 glm::mat4 pMat, vMat, mMat, mvMat;
 
 glm::mat4 tMat, rMat;	////	added two matrices - translation and rotation for using current time to compute different translations in x, y, and z
@@ -49,63 +50,53 @@ void setupVertices(void) {						//2)
 }
 void init(GLFWwindow* window) {					//1)init() performs tasks that only need to be done once:
 	renderingProgram = Utils::createShaderProgram("vertShader.glsl", "fragShader.glsl");	//	reading in shader code and building the rendeering program
-	cameraX = 0.0f; cameraY = 0.0f; cameraZ = 8.0f;
-	cubeLocX = 0.0f; cubeLocY = -2.0f; cubeLocZ = 0.0f; /// shift down Y to reveal perspective
+	glfwGetFramebufferSize(window, &width, &height);
+	aspect = (float)width / (float)height;
+	pMat = glm::perspective(1.0472f, aspect, 0.1f, 1000.0f); // 1.0472 radians == 60 degrees
+
+	// position the camera further down the positive Z axis (to see all of the cubes)
+	cameraX = 0.0f; cameraY = 0.0f; cameraZ = 32.0f;
 	setupVertices();																		//	loading cube vertices into the VBO	
 }
 void display(GLFWwindow* window, double currentTime) {	//3)display() may be called	repeatedly and the rate at which it is called is referred to as the frame rate
 	glClear(GL_DEPTH_BUFFER_BIT);	//	usually necessary to clear the depth buffer before rendering a frame so that hidden surface removal occurs properly
-
+	glClearColor(0.0, 0.0, 0.0, 1.0);
 	glClear(GL_COLOR_BUFFER_BIT);	////	as we added color changing we need to clear the color buffer too
 
 	glUseProgram(renderingProgram);	//	enabling shaders by installing the GLSL code on the GPU.
 									//	 this doesn’t run the shader program, but it does enable subsequent OpenGL calls to determine the shader’s vertex attribute and uniform locations
-	/// get the uniform variables for the MV and projection matrices
-	mvLoc = glGetUniformLocation(renderingProgram, "mv_matrix");	//	 get locations of uniforms in the shader program
-	projLoc = glGetUniformLocation(renderingProgram, "proj_matrix");
-	/// build perspective matrix
-	glfwGetFramebufferSize(window, &width, &height);
-	aspect = (float)width / (float)height;
-	pMat = glm::perspective(1.0472f, aspect, 0.1f, 1000.0f); // 1.0472 radians = 60 degrees
-	/// build view matrix, model matrix, and model-view matrix
+	///////////////computations that build (and transform) mMat have been moved to the vertex shader
+	///////////////there is no longer any need to build an MV matrix in the C++ application.
+
+
 	vMat = glm::translate(glm::mat4(1.0f), glm::vec3(-cameraX, -cameraY, -cameraZ));
 
-	for (displayLoopi = 0; displayLoopi < 24; displayLoopi++)
-	{
-		tf = currentTime + displayLoopi; // tf == "time factor", declared as type float
+	vLoc = glGetUniformLocation(renderingProgram, "v_matrix");
+	projLoc = glGetUniformLocation(renderingProgram, "proj_matrix");
 
-	////// use current time to compute different translations in x, y, and z
-		tMat = glm::translate(glm::mat4(1.0f), glm::vec3(sin(.35f * tf) * 8.0f, cos(.52f * tf) * 8.0f,
-			sin(.70f * tf) * 8.0f));
+	glUniformMatrix4fv(vLoc, 1, GL_FALSE, glm::value_ptr(vMat));
+	glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(pMat));
 
-		rMat = glm::rotate(glm::mat4(1.0f), 1.75f * (float)currentTime, glm::vec3(0.0f, 1.0f, 0.0f));
-		rMat = glm::rotate(rMat, 1.75f * (float)currentTime, glm::vec3(1.0f, 0.0f, 0.0f));
-		rMat = glm::rotate(rMat, 1.75f * (float)currentTime, glm::vec3(0.0f, 0.0f, 1.0f));
-		////// the 1.75 adjusts the rotation speed
-		mMat = tMat * rMat;	////	rotation initially -> next the translation performed
 
-		////mMat = glm::translate(glm::mat4(1.0f), glm::vec3(cubeLocX, cubeLocY, cubeLocZ));
+	timeFactor = ((float)currentTime);
+	tfLoc = glGetUniformLocation(renderingProgram, "tf");
+	glUniform1f(tfLoc, (float)timeFactor);
 
-		mvMat = vMat * mMat;
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+	glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, 0);
+	glEnableVertexAttribArray(0);
 
-		/// copy perspective and MV matrices to corresponding uniform variables
-		glUniformMatrix4fv(mvLoc, 1, GL_FALSE, glm::value_ptr(mvMat));	//	send matrix data to the uniform variables
-		glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(pMat));
-		/// associate VBO with the corresponding vertex attribute in the vertex shader
-		glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);	//	enables the buffer containing the cube vertices
-		glEnableVertexAttribArray(0);							//	and attaches it to 0th vertex attribute to prepare for sending the vertices to the shader.
-		/// adjust OpenGL settings and draw model
-		glEnable(GL_DEPTH_TEST);	//	enable depth testing
-		glDepthFunc(GL_LEQUAL);		//	and specify the particular depth test we wish OpenGL to use
-		glDrawArrays(GL_TRIANGLES, 0, 36);
-	}
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LEQUAL);
+
+	glDrawArraysInstanced(GL_TRIANGLES, 0, 36, 24);
+
 }
 int main(void) { /// main() is unchanged from before
 	if (!glfwInit()) { exit(EXIT_FAILURE); }
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	GLFWwindow* window = glfwCreateWindow(1080, 720, "Chapter 4 - program 1", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(600, 600, "Chapter 4 - program 1", NULL, NULL);
 	glfwMakeContextCurrent(window);
 	if (glewInit() != GLEW_OK) { exit(EXIT_FAILURE); }
 	glfwSwapInterval(1);
