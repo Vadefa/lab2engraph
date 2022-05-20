4857cf656cca292d8aae53b9ea3f28ca081efb5d
Вадим Сухинин <suhinin.vadim2002@yandex.ru>
2022-05-20 11:12:39 +0500

    copied

--- lab2engraph.cpp
+++ lab2engraph.cpp
@@ -1,163 +1,96 @@
-﻿#include <iostream>
+﻿#include <GL\glew.h>
+#include <GLFW\glfw3.h>
 #include <string>
-#include <GL/glew.h>
-#include <GL/freeglut.h>
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <glm\glm.hpp>
+#include <glm\gtc\type_ptr.hpp>
+#include <glm\gtc\matrix_transform.hpp>
+#include "Utils.h"
 
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
+using namespace std;
+#define numVAOs 1
+#define numVBOs 2
+float cameraX, cameraY, cameraZ;
+float cubeLocX, cubeLocY, cubeLocZ;
+GLuint renderingProgram;
+GLuint vao[numVAOs];
+GLuint vbo[numVBOs];
+// allocate variables used in display() function, so that they won’t need to be allocated during rendering
+GLuint mvLoc, projLoc;
+int width, height;
+float aspect;
+glm::mat4 pMat, vMat, mMat, mvMat;
+void setupVertices(void) { // 36 vertices, 12 triangles, makes 2x2x2 cube placed at origin
+	float vertexPositions[108] = {
+	 -1.0f, 1.0f, -1.0f, -1.0f, -1.0f, -1.0f, 1.0f, -1.0f, -1.0f,
+	 1.0f, -1.0f, -1.0f, 1.0f, 1.0f, -1.0f, -1.0f, 1.0f, -1.0f,
+	 1.0f, -1.0f, -1.0f, 1.0f, -1.0f, 1.0f, 1.0f, 1.0f, -1.0f,
+	 1.0f, -1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, -1.0f,
+	 1.0f, -1.0f, 1.0f, -1.0f, -1.0f, 1.0f, 1.0f, 1.0f, 1.0f,
+	 -1.0f, -1.0f, 1.0f, -1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f,
+	 -1.0f, -1.0f, 1.0f, -1.0f, -1.0f, -1.0f, -1.0f, 1.0f, 1.0f,
+	 -1.0f, -1.0f, -1.0f, -1.0f, 1.0f, -1.0f, -1.0f, 1.0f, 1.0f,
+	 -1.0f, -1.0f, 1.0f, 1.0f, -1.0f, 1.0f, 1.0f, -1.0f, -1.0f,
+	 1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f, 1.0f,
+	 -1.0f, 1.0f, -1.0f, 1.0f, 1.0f, -1.0f, 1.0f, 1.0f, 1.0f,
+	 1.0f, 1.0f, 1.0f, -1.0f, 1.0f, 1.0f, -1.0f, 1.0f, -1.0f
+	};
+	glGenVertexArrays(1, vao);
+	glBindVertexArray(vao[0]);
+	glGenBuffers(numVBOs, vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertexPositions), vertexPositions, GL_STATIC_DRAW);
+}
+void init(GLFWwindow* window) {
+	renderingProgram = Utils::createShaderProgram("vertShader.glsl", "fragShader.glsl");
+	cameraX = 0.0f; cameraY = 0.0f; cameraZ = 8.0f;
+	cubeLocX = 0.0f; cubeLocY = -2.0f; cubeLocZ = 0.0f; // shift down Y to reveal perspective
+	setupVertices();
+}
+void display(GLFWwindow* window, double currentTime) {
+	glClear(GL_DEPTH_BUFFER_BIT);
+	glUseProgram(renderingProgram);
+	// get the uniform variables for the MV and projection matrices
+	mvLoc = glGetUniformLocation(renderingProgram, "mv_matrix");
+	projLoc = glGetUniformLocation(renderingProgram, "proj_matrix");
+	// build perspective matrix
+	glfwGetFramebufferSize(window, &width, &height);
+	aspect = (float)width / (float)height;
+	pMat = glm::perspective(1.0472f, aspect, 0.1f, 1000.0f); // 1.0472 radians = 60 degrees
+	// build view matrix, model matrix, and model-view matrix
+	vMat = glm::translate(glm::mat4(1.0f), glm::vec3(-cameraX, -cameraY, -cameraZ));
+	mMat = glm::translate(glm::mat4(1.0f), glm::vec3(cubeLocX, cubeLocY, cubeLocZ));
+	mvMat = vMat * mMat;
 
-//код скрипта вершинного шейдера:
-static const char* pVS = "                                                    \n\
-#version 330                                                                  \n\
-uniform mat4 gWorld;														  \n\
-                                                                              \n\
-layout (location = 0) in vec3 Position;                                       \n\
-                                                                              \n\
-void main()                                                                   \n\
-{                                                                             \n\
-	gl_Position = gWorld * vec4(Position, 1.0);								  \n\
-}";
-
-//код скрипта фрагментного шейдера
-static const char* pFS = "                                                    \n\
-#version 330                                                                  \n\
-                                                                              \n\
-out vec4 FragColor;                                                           \n\
-                                                                              \n\
-void main()                                                                   \n\
-{                                                                             \n\
-    FragColor = vec4(1.0, 0.0, 0.0, 1.0);                                     \n\
-}";
-
-GLuint VBO;
-GLuint gWorldLocation;
-
-void RenderSceneCB() {
-
-	glClear(GL_COLOR_BUFFER_BIT);
-
-	//статичный размер создаётся здесь и удаляется лишь при завершении программы
-	static float Scale = 0.0f;
-	Scale += 0.001f;
-
-	//подготавливаем матрицу для плавного изменения треугольником значения координаты х. Остальные координаты 3-го столбца = 0
-	glm::mat4x4 World{};
-	World[0][0] = 1.0f; World[0][1] = 0.0f; World[0][2] = 0.0f; World[0][3] = sinf(Scale);
-	World[1][0] = 0.0f; World[1][1] = 1.0f; World[1][2] = 0.0f; World[1][3] = 0.0f;
-	World[2][0] = 0.0f; World[2][1] = 0.0f; World[2][2] = 1.0f; World[2][3] = 0.0f;
-	World[3][0] = 0.0f; World[3][1] = 0.0f; World[3][2] = 0.0f; World[3][3] = 1.0f;
-
-	glUniformMatrix4fv(gWorldLocation, 1, GL_TRUE, &World[0][0]);
-
-	glEnableVertexAttribArray(0);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-
+	// copy perspective and MV matrices to corresponding uniform variables
+	glUniformMatrix4fv(mvLoc, 1, GL_FALSE, glm::value_ptr(mvMat));
+	glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(pMat));
+	// associate VBO with the corresponding vertex attribute in the vertex shader
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-	glDrawArrays(GL_TRIANGLES, 0, 3);
-	glDisableVertexAttribArray(0);
-
-	glutSwapBuffers();
-	glutPostRedisplay(); // останавливает повторный вызов ленивой функции
+	glEnableVertexAttribArray(0);
+	// adjust OpenGL settings and draw model
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LEQUAL);
+	glDrawArrays(GL_TRIANGLES, 0, 36);
 }
-
-
-static void createVertexBuffer() {
-	glm::vec3 vecArrTrngl[3];
-	vecArrTrngl[0] = glm::vec3(-1.0f, -1.0f, 0.0f);
-	vecArrTrngl[1] = glm::vec3(1.0f, -1.0f, 0.0f);
-	vecArrTrngl[2] = glm::vec3(0.0f, 1.0f, 0.0f);
-
-	glGenBuffers(1, &VBO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vecArrTrngl), vecArrTrngl, GL_STATIC_DRAW);
-
-}
-
-static void createShader(GLuint ShaderProgram, const char* pShaderText, GLenum ShaderType) {
-	//GLuint ShaderProgram = glCreateProgram();
-
-	GLuint ShaderObj = glCreateShader(ShaderType);
-
-	const GLchar* p[1];
-	p[0] = pShaderText;
-	GLint Lengths[1];
-	Lengths[0] = strlen(pShaderText);
-	glShaderSource(ShaderObj, 1, p, Lengths);
-
-	glCompileShader(ShaderObj); 
-
-	GLint success;
-	glGetShaderiv(ShaderObj, GL_COMPILE_STATUS, &success);
-	if (!success) {
-		GLchar InfoLog[1024];
-		glGetShaderInfoLog(ShaderObj, sizeof(InfoLog), NULL, InfoLog);
-		fprintf(stderr, "Error compiling shader type %d: '%s'\n", ShaderType, InfoLog);
+int main(void) { // main() is unchanged from before
+	if (!glfwInit()) { exit(EXIT_FAILURE); }
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	GLFWwindow* window = glfwCreateWindow(600, 600, "Chapter 4 - program 1", NULL, NULL);
+	glfwMakeContextCurrent(window);
+	if (glewInit() != GLEW_OK) { exit(EXIT_FAILURE); }
+	glfwSwapInterval(1);
+	init(window);
+	while (!glfwWindowShouldClose(window)) {
+		display(window, glfwGetTime());
+		glfwSwapBuffers(window);
+		glfwPollEvents();
 	}
-
-	glAttachShader(ShaderProgram, ShaderObj);
-}
-
-static void compileShaders() {
-
-	//нужно поработать с шейдерами
-
-	GLuint ShaderProgram = glCreateProgram();
-
-	createShader(ShaderProgram, pVS, GL_VERTEX_SHADER);
-	createShader(ShaderProgram, pFS, GL_FRAGMENT_SHADER);
-
-	glLinkProgram(ShaderProgram);
-
-	GLint Success = 0;					// проверяем на успешную линковку
-	GLchar ErrorLog[1024] = { 0 };
-	glGetProgramiv(ShaderProgram, GL_LINK_STATUS, &Success);
-	if (Success == 0) {
-		glGetProgramInfoLog(ShaderProgram, sizeof(ErrorLog), NULL, ErrorLog);
-		fprintf(stderr, "Error linking shader program: '%s'\n", ErrorLog);
-	}
-
-	glValidateProgram(ShaderProgram);
-
-	glUseProgram(ShaderProgram);
-
-	//gScaleLocation = glGetUniformLocation(ShaderProgram, "gScale");
-	//assert(gScaleLocation != 0xFFFFFFFF);
-	gWorldLocation = glGetUniformLocation(ShaderProgram, "gWorld");
-	assert(gWorldLocation != 0xFFFFFFFF);
-}
-
-
-
-int main(int argc, char** argv)
-{
-	glutInit(&argc, argv);
-
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
-
-	glutInitWindowSize(1024, 768);
-	glutInitWindowPosition(100, 100);
-	glutCreateWindow("Lab 2");
-
-	glutDisplayFunc(RenderSceneCB);
-
-	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-
-	GLenum res = glewInit();
-	if (res != GLEW_OK)
-	{
-		fprintf(stderr, "Error: '%s'\n", glewGetErrorString(res));
-		return 1;
-	}
-
-	createVertexBuffer();
-
-	////////lab 2//////////
-	//сначала зададим шейдеры и uniform-переменную Scale
-	compileShaders();
-
-	//теперь нужно задать ленивую функцию
-	glutIdleFunc(RenderSceneCB);
-
-	glutMainLoop();
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	exit(EXIT_SUCCESS);
 }
